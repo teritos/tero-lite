0afea3def31931509edc425154cea825d8eea5f7
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2018-02-06 17:19:18 -0300

    saco ftp server y telegram bot del dir apps, actualizo el settings para soportar django

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,91 @@
 
 import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'secret') 
 DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'db',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+
+SQLITE_PATH = os.getenv('SQLITE_PATH', 'tero.sqlite')  # TODO: Deprecate
+
 FTP_FILES = os.getenv('FTP_FILES', '/tmp')
-SQLITE_PATH = os.getenv('SQLITE_PATH', 'tero.sqlite')
+
+AUTH_USER_MODEL = 'db.Account'
+
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+FTP_FILES = os.path.join(BASE_DIR, 'ftp-files')
